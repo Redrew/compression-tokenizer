c6678d74ca433f1d0a58ad5e3d739c271b1424d8
Redrew <andrewshen2913@gmail.com>
2023-10-23 17:25:23 -0400

    change dataloader to support zipping and huggingface datasets

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 from MEGABYTE_pytorch import MEGABYTE
 
+import os
+import sys
 import random
 import tqdm
 import gzip
@@ -11,14 +13,17 @@ from torch.utils.data import DataLoader, Dataset
 
 # constants
 
+EXP_NAME = sys.argv[1]
+ZIPPED = False
 NUM_BATCHES = int(1e5)
-BATCH_SIZE = 4
-GRADIENT_ACCUMULATE_EVERY = 4
+BATCH_SIZE = 16
+GRADIENT_ACCUMULATE_EVERY = 1
 LEARNING_RATE = 2e-4
 VALIDATE_EVERY  = 100
 GENERATE_EVERY  = 500
 PRIME_LEN = 100
 SEQ_LEN = 8192
+ENABLE_DP = True
 
 # helpers
 
@@ -33,6 +38,19 @@ def decode_token(token):
 def decode_tokens(tokens):
     return ''.join(list(map(decode_token, tokens)))
 
+# logging
+class Logger(object):
+    def __init__(self, f):
+        self.terminal = sys.stdout
+        self.log = open(f, "w")
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+    def flush(self):
+        pass
+os.makedirs(f"outputs/{EXP_NAME}", exist_ok=True)
+sys.stdout = Logger(f"outputs/{EXP_NAME}/log.txt")
+
 # instantiate GPT-like decoder model
 
 model = MEGABYTE(
@@ -43,35 +61,55 @@ model = MEGABYTE(
     flash_attn = True
 ).cuda()
 
-# prepare enwik8 data
-
-with gzip.open('./data/enwik8.gz') as file:
-    x = np.frombuffer(file.read(int(95e6)), dtype=np.uint8).copy()
-    train_x, valid_x = np.split(x, [int(90e6)])
-    data_train, data_val = map(torch.from_numpy, (train_x, valid_x))
+# prepare data
+from datasets import load_dataset
+import io
+split = "train"
+dataset = load_dataset("pg19")
 
 class TextSamplerDataset(Dataset):
-    def __init__(self, data, seq_len):
+    def __init__(self, data, seq_len, zipped=False):
         super().__init__()
+        self.zip_multiplier = 4 if zipped else 1
         self.data = data
         self.seq_len = seq_len
+        self.doc_lengths = np.array([len(doc["text"]) for doc in data])
+        self.doc_lengths[self.doc_lengths <= self.seq_len * self.zip_multiplier] = 0
+        self.zipped = zipped
 
     def __getitem__(self, index):
-        rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
-        full_seq = self.data[rand_start: rand_start + self.seq_len].long()
+        rand_doc = np.random.choice(len(self.doc_lengths), p=self.doc_lengths/self.doc_lengths.sum())
+        text = self.data[rand_doc]["text"]
+        # sample a longer piece of text if we are zipping
+        text_seq_len = self.seq_len * self.zip_multiplier
+        rand_start = torch.randint(0, len(text) - text_seq_len, (1,))
+        bytes = text[rand_start: rand_start + text_seq_len].encode("utf-8")
+        if self.zipped:
+            buffer = io.BytesIO()
+            with gzip.GzipFile(fileobj=buffer, mode='wb') as f:
+                f.write(bytes)
+            bytes = buffer.getvalue()
+            assert len(bytes) >= self.seq_len, f"zipped data too small {len(bytes)}"
+        full_seq = torch.LongTensor(np.frombuffer(bytes[:self.seq_len], dtype=np.uint8).copy())
         return full_seq.cuda()
 
     def __len__(self):
-        return self.data.size(0) // self.seq_len
+        return self.doc_lengths.sum() // self.seq_len
 
-train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
-val_dataset   = TextSamplerDataset(data_val, SEQ_LEN)
+train_dataset = TextSamplerDataset(dataset["train"], SEQ_LEN, zipped=ZIPPED)
+val_dataset   = TextSamplerDataset(dataset["validation"], SEQ_LEN, zipped=ZIPPED)
 train_loader  = cycle(DataLoader(train_dataset, batch_size = BATCH_SIZE))
 val_loader    = cycle(DataLoader(val_dataset, batch_size = BATCH_SIZE))
 
 # optimizer
 
 optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# multi-gpu training
+if ENABLE_DP:
+    devices = list(range(torch.cuda.device_count()))
+    model = torch.nn.DataParallel(model, device_ids=devices)
+    model.generate = model.module.generate
 
 # training
 
@@ -80,6 +118,8 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
 
     for __ in range(GRADIENT_ACCUMULATE_EVERY):
         loss = model(next(train_loader), return_loss = True)
+        if ENABLE_DP:
+            loss = loss.mean()
         loss.backward()
 
     print(f'training loss: {loss.item()}')
@@ -91,6 +131,8 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
         model.eval()
         with torch.no_grad():
             loss = model(next(val_loader), return_loss = True)
+            if ENABLE_DP:
+                loss = loss.mean()
             print(f'validation loss: {loss.item()}')
 
     if i != 0 and i % GENERATE_EVERY == 0:
@@ -105,3 +147,5 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
 
         output_str = decode_tokens(sample[0][PRIME_LEN:])
         print(output_str)
+
+torch.save(model, f"outputs/{EXP_NAME}/model.pt")

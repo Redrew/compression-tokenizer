8a585e0134375537f31a56afc99970b79c4649f0
vkrd <vikduvvur@gmail.com>
2023-12-04 21:05:17 +0000

    working code for eval images

--- image_eval.py
+++ image_eval.py
@@ -4,19 +4,19 @@ from omegaconf import OmegaConf
 import os
 import sys
 from datasets import load_dataset
-from train import MNISTDataset
+from train import MNISTDataset, RLE
 import torch
 from tqdm import tqdm
 
 config = OmegaConf.load(sys.argv[1])
-IMAGES_TO_SAVE = 20
+IMAGES_TO_SAVE = 0
 PERCENT_EXISTS = 0.5
-
+MAX_TOKENS = 784
 
 def reverse_rle(bytes):
     result = []
-    for i in range(0, len(bytes), 2):
-        result.extend([bytes[i]] * bytes[i+1])
+    for i in range(len(bytes)//2):
+        result.extend([bytes[i*2]] * bytes[(i*2)+1])
     return torch.tensor(result, dtype=torch.uint8)
 
 def convert_to_image(data):
@@ -35,31 +35,48 @@ dataset = load_dataset("mnist")
 mnist = MNISTDataset(dataset["test"], device=device)
 pixels_to_keep = int(784 * PERCENT_EXISTS)
 
-if IMAGES_TO_SAVE > 0:
-    os.makedirs(f"outputs/{config.exp_name}/images", exist_ok=True)
-    if PERCENT_EXISTS == 0.0:
-        raise ValueError("PERCENT_EXISTS cannot be 0.0")
-    else:
-        for i in range(IMAGES_TO_SAVE):
-            set_pixels = mnist.__getitem__(i)[:pixels_to_keep]
-            model_output = model(set_pixels, return_loss=False) # Need to check this line
-            autocompleted_image = reverse_rle(model_output)
-            full_image =  torch.concatenate((set_pixels, autocompleted_image))
-            image = convert_to_image(full_image)
-            plt.imsave(f"outputs/{config.exp_name}/images/{PERCENT_EXISTS}_{i}.png", image, cmap="gray")
+with torch.no_grad():
+    if IMAGES_TO_SAVE > 0:
+        os.makedirs(f"outputs/{config.exp_name}/images", exist_ok=True)
+        if PERCENT_EXISTS == 0.0:
+            raise ValueError("PERCENT_EXISTS cannot be 0.0")
+        else:
 
-mse = 0.0
 
-for i in tqdm(range(10_000)):
-    target_image = mnist.__getitem__(i)
-    set_pixels = mnist.__getitem__(i)
-    model_output = model(set_pixels, return_loss=False)
-    autocompleted_image = reverse_rle(model_output)
-    full_image =  torch.concatenate((set_pixels, autocompleted_image))
-    image = convert_to_image(full_image)
-    mse += np.mean((image - set_pixels).float()**2)
+            for i in tqdm(range(IMAGES_TO_SAVE)):
+                set_pixels = mnist.__getitem__(i)[:pixels_to_keep]
+                set_pixels = torch.tensor(RLE(set_pixels)).reshape(1, -1).to(device)
+                
+                for _ in range(MAX_TOKENS - len(set_pixels)):
+                    model_output = model(set_pixels)
+                    model_predictions = model_output.argmax(dim=-1)[0][-1]
+                    set_pixels = torch.cat((set_pixels, torch.tensor([[model_predictions]]).to(device)), dim=1)
 
-print(f"Average MSE: {mse / 10_000}")
-print(f"Average PSNR: {20 * np.log10(255) - 10 * np.log10(mse / 10_000)}")
-import math
-print(f"Average PSNR (not copilot written)", 10 * math.log10(255**2 / (mse / 10_000)))
+                autocompleted_image = reverse_rle(set_pixels[0].cpu())
+                
+                image = convert_to_image(autocompleted_image)
+                plt.imsave(f"outputs/{config.exp_name}/images/{PERCENT_EXISTS}_{i}.png", image, cmap="gray")
+
+    mse = 0.0
+
+    BATCH_SIZE = 20
+    for i in tqdm(range(10_000/BATCH_SIZE)):
+        target_image = mnist.__getitem__(i).cpu().numpy()
+        set_pixels = mnist.__getitem__(i)[:pixels_to_keep]
+        set_pixels = torch.tensor(RLE(set_pixels)).reshape(1, -1).to(device)
+        
+        for _ in range(MAX_TOKENS - len(set_pixels)):
+            model_output = model(set_pixels)
+            model_predictions = model_output.argmax(dim=-1)[0][-1]
+            set_pixels = torch.cat((set_pixels, torch.tensor([[model_predictions]]).to(device)), dim=1)
+
+        set_pixels = set_pixels[0].cpu().numpy()
+        autocompleted_image = reverse_rle(set_pixels)
+        
+        image = convert_to_image(autocompleted_image)
+        
+        mse += np.mean((image.flatten() - target_image.flatten()).astype(np.float16)**2)*2
+        break
+
+    print(f"Average MSE: {mse / 10_000}")
+    print(f"Average PSNR: {20 * np.log10(255) - 10 * np.log10(mse / 10_000)}")
